073b96a62d85f5ad755bb7c33a2e9a7b77c8bfdd
Beate Skarsta <beate9098@gmail.com>
2024-07-11 22:29:48 +0200

    created basic header

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
+
+
 
 export default function DefaultLayoutPage() {
-  return (
-    <>
-      <Outlet/>
-    </>
-  )
+    return (
+        <>
+            <Outlet/>
+        </>
+    )
 }
--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -1,13 +1,19 @@
-import { useSelector } from 'react-redux'
-import { Navigate, Outlet } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {Navigate, Outlet} from 'react-router-dom'
+
+
 
 export default function ProtectedRoute() {
 
     const isLoggedIn = useSelector((state) => state.user.accessToken)
 
-    if(isLoggedIn){
-        return <Outlet/>
+    if (isLoggedIn) {
+        return (
+            <>
+                <Outlet/>
+            </>
+        );
     } else {
-        return <Navigate to='/login' />
+        return <Navigate to='/login'/>
     }
 }
