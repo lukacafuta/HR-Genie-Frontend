680d6bd5645e1322973ac36f8a834005ba0209c0
Alexander Stöhr <git@alexander-stoehr.de>
2024-07-13 22:00:27 +0200

    added basic form elements

--- src/routes/company.jsx
+++ src/routes/company.jsx
@@ -1,4 +1,5 @@
 import {RouteContentStyled, RouteHeadStyled} from "../styles/routeGeneralStyles.js";
+import {CompanyFieldsStyled, WorkingHoursStyled} from "../styles/companySettingsStyles.js";
 
 export default function CompanyRoute() {
     return (
@@ -7,6 +8,29 @@ export default function CompanyRoute() {
                 <div><h2>Company Settings</h2>
                     <p>Add something here</p></div>
             </RouteHeadStyled>
+
+            <CompanyFieldsStyled>
+                <div>
+                    <h3>Company Name</h3>
+                    <input type="text" placeholder="Company Name"/>
+                </div>
+                <div>
+                    <h3>Company Logo</h3>
+                    <input type="file" placeholder="Upload Logo"/>
+                </div>
+                <WorkingHoursStyled>
+                    <h3>Working Hours</h3>
+                    <div>
+                        Start:
+                        <input type="time" placeholder="Opening Time"/>
+                    </div>
+                    <div>
+                        End:
+                        <input type="time" placeholder="Closing Time"/>
+                    </div>
+                </WorkingHoursStyled>
+            </CompanyFieldsStyled>
+
         </RouteContentStyled>
     )
 }
--- src/store/slices/ViewSlice.jsx
+++ src/store/slices/ViewSlice.jsx
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit";
 const ViewSlice = createSlice({
     name: "view",
     initialState: {
-        view: "employee",
+        view: "company_admin",
     },
     reducers: {
         changeView: (state, action) => {
