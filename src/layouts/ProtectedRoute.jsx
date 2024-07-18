cdff9731398548b56be8f910a88fad40cb750e36
Alexander Stöhr <git@alexander-stoehr.de>
2024-07-18 21:12:58 +0200

    added JWT token verify

--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -1,11 +1,34 @@
-import {useSelector} from 'react-redux'
-import {Navigate, Outlet} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {Navigate, Outlet, useNavigate} from 'react-router-dom'
+import {useEffect} from "react";
+import {api} from "../common/api.js";
+import {login} from "../store/slices/UserSlice.jsx";
 
 export default function ProtectedRoute({allowedRoles}) {
 
     const isLoggedIn = useSelector((state) => state.user.accessToken)
     const userRole = useSelector((state) => state.user.role)
     console.log("role: ", userRole)
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const localToken = localStorage.getItem("accessToken");
+        if (localToken) {
+            console.log("token there");
+            api.post("auth/token/verify/", {token: localToken})
+                .then(dispatch(login(localToken)))
+                .catch(() => {
+                    localStorage.removeItem("accessToken");
+                    navigate("/login");
+                    console.log("wrong token");
+                });
+        } else {
+            console.log("no token");
+            navigate("/login");
+        }
+    }, []);
 
     if (!isLoggedIn) {
         return <Navigate to='/login'/>;
