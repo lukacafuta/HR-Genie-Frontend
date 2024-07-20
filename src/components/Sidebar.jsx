4855b81928bca3d84e670ff7c762918459e9ea4a
Alexander Stöhr <git@alexander-stoehr.de>
2024-07-20 18:16:19 +0200

    fixed view change navigate and sidebar text

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ export default function Sidebar() {
                                 d="M12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14Z"
                                 stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
-                        Company Settings</NavLinkButtonStyled>
+                    </NavLinkButtonStyled>
                     <NavLinkButtonStyled to="/company/employees">
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
