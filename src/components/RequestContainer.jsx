125c1272a5d3887a7c24e646705e815f7dc2dd39
Quentin Lamare <1092-qlaCode@users.noreply.gitlab.propulsion-home.ch>
2024-07-22 01:22:01 +0200

    in employee and manager views: GET Requests - works; POST Requests - works

--- src/components/RequestContainer.jsx
+++ src/components/RequestContainer.jsx
@@ -1,25 +1,48 @@
 import { useDispatch, useSelector } from "react-redux";
 import RequestCard from "./RequestCard";
-import { useEffect } from "react";
-import { initialLoadLocalStorage } from "../store/slices/RequestSlice";
+import { useEffect, useState } from "react";
+import { loadRequests } from "../store/slices/RequestSlice";
 import { RowCardContainer } from "../styles/cardStyles.js";
+import { api } from "../common/api.js";
 
-export default function RequestContainer() {
+export default function RequestContainer({ refresh }) {
   const dispatch = useDispatch();
+  const view = useSelector((state) => state.view.view);
+  const requestList = useSelector((state) => state.request.requestList);
 
-  //while there is no data
-  const initialRequestList = useSelector((state) => state.request.requestList);
+  const accessToken = useSelector((state) => state.user.accessToken);
+  const [hasFetchedAbsences, setHasFetchedAbsences] = useState(false);
+
+  const fetchRequests = () => {
+    const storeToken = accessToken;
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${storeToken}`,
+      },
+    };
+
+    try {
+      // Is the user in Manager or Employee View
+      let endpointForAbsences =
+        view === "manager" ? "/absences/" : "/absences/me/";
+      console.log("yep i am here");
+      api.get(endpointForAbsences, config).then((res) => {
+        console.log("API call successful", res.data);
+        let requestData = res.data;
+        dispatch(loadRequests(requestData));
+      });
+    } catch (error) {
+      console.log("nope");
+      console.error(error);
+    }
+  };
+
+  // created to refresh the requests
 
   useEffect(() => {
-    const storedRequestList = JSON.parse(localStorage.getItem("requestList"));
-    if (storedRequestList && storedRequestList.length > 0) {
-      dispatch(initialLoadLocalStorage(storedRequestList));
-    } else {
-      localStorage.setItem("requestList", JSON.stringify(initialRequestList));
-    }
-  }, [dispatch]);
-
-  const requestList = useSelector((state) => state.request.requestList);
+    fetchRequests();
+  }, [dispatch, refresh]);
 
   return (
     <>
