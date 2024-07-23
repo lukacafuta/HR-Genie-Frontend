aacb5668152f105777c11d322d43b592d380c1e6
Alexander Stöhr <git@alexander-stoehr.de>
2024-07-23 15:52:20 +0200

    added error message for failed login

--- src/styles/loginStyles.js
+++ src/styles/loginStyles.js
@@ -4,9 +4,8 @@ import styled from "styled-components";
 export const LoginPageStyled = styled.div`
     display: flex;
     justify-content: center;
-    align-items: center;
-    height: 100vh;
-    padding-bottom: 4rem;
+    //height: 100vh;
+    padding: 8rem 0 4rem 0;
 `;
 export const LoginContainerStyled = styled.div`
     display: flex;
@@ -14,7 +13,7 @@ export const LoginContainerStyled = styled.div`
     justify-content: center;
     align-items: center;
     background-color: var(--white);
-    padding: 0 2rem 4rem 2rem;
+    padding: 0 2rem 2rem 2rem;
     max-width: 500px;
     border-radius: var(--border-radius-large);
     box-shadow: var(--inputs-shadow);
@@ -63,5 +62,26 @@ export const LoginContainerStyled = styled.div`
         margin-bottom: 0.5rem;
         font-weight: 300;
         color: var(--light-grey);
+    }
+`;
+
+export const ErrorMessageStyled = styled.div`
+    visibility: hidden;
+    overflow: hidden;
+    color: var(--red-button-color);
+    border: 1px solid var(--red-button-color);
+    background-color: rgba(255, 0, 0, 0.09);
+    border-radius: 8px;
+    padding: 0.5rem;
+    margin-top: 1rem;
+    font-weight: 500;
+    text-align: center;
+    z-index: 100;
+    height: 0;
+
+    &.errorMessageVisible {
+        visibility: visible;
+        height: auto;
+        overflow: visible;
     }
 `;
