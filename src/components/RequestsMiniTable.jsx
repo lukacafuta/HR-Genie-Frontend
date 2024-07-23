3ba6cebddc4aa6b956b7e7acd8ec019abbf3226b
Alexander Stöhr <git@alexander-stoehr.de>
2024-07-23 12:07:16 +0200

    fixed minitables by splitting the lists into training and requests

--- src/components/RequestsMiniTable.jsx
+++ src/components/RequestsMiniTable.jsx
@@ -4,14 +4,26 @@ import ButtonBrand from "./buttons/ButtonBrand.jsx";
 
 export default function RequestMiniTable({name, type, requests}) {
 
-    const filteredRequests = requests.filter(request => request.reason === type);
-    console.log(type, "minitable comp: ", requests)
+    const isTrainingRequestlist = requests[0].hasOwnProperty("trainingUrl")
+    // console.log(isTrainingRequestlist)
+    // const filteredRequests = requests.filter(request => request.reason === type);
+    // console.log(type, "minitable comp: ", requests)
+
+    let filteredRequests
+
+    if (isTrainingRequestlist) {
+        filteredRequests = requests.filter(request => request.trainingUrl);
+        console.log(filteredRequests)
+    } else {
+        filteredRequests = requests.filter(request => request.reason === type);
+    }
 
 
     return (
         <RequestsMinitableStyled>
             <div><h3>{name}</h3>
-                {filteredRequests.map((request, i) => <RequestMinitableItem key={i} request={request}/>)}</div>
+                {filteredRequests.map((request, i) => <RequestMinitableItem key={i} isTraining={isTrainingRequestlist} request={request}/>)}
+            </div>
             <ButtonBrand buttonText={'View all'} iconURL={''}/>
         </RequestsMinitableStyled>
     );
