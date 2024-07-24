3003248f7180b194bf0c739eb4eb0aae1279e504
Quentin Lamare <1092-qlaCode@users.noreply.gitlab.propulsion-home.ch>
2024-07-25 00:51:02 +0200

    Employee List + EmployeeCard in it

--- src/routes/companyEmployees.jsx
+++ src/routes/companyEmployees.jsx
@@ -1,35 +1,63 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
 import ButtonBrand from "../components/buttons/ButtonBrand.jsx";
 import EmployeeForm from "../components/forms/EmployeeForm.jsx";
 import {
-    RouteContentStyled,
-    RouteHeadStyled,
+  RouteContentStyled,
+  RouteHeadStyled,
 } from "../styles/routeGeneralStyles.js";
 import UserContainer from "../components/UserContainer.jsx";
+import { api } from "../common/api.js";
+import { loadTrainings } from "../store/slices/RequestSlice.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserList } from "../store/slices/UserSlice.jsx";
 
 export default function CompanyEmployeesRoute() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  const accessToken = useSelector((state) => state.user.accessToken);
 
-    return (
-        <RouteContentStyled>
-            <RouteHeadStyled>
-                <div>
-                    <h2>Employees</h2>
-                    <p>Add something here</p>
-                </div>
-                {/*<ButtonBrand iconURL="/plus-add.svg" buttonText="Add Employee"/>*/}
-                <ButtonBrand
-                    iconURL={"/plus-add.svg"}
-                    buttonText={"Add Employee"}
-                    onClick={openModal}
-                />
-                <EmployeeForm isModalOpen={isModalOpen} closeModal={closeModal}/>
-                {/*</div>*/}
-            </RouteHeadStyled>
-            <UserContainer/>
-        </RouteContentStyled>
-    );
+  const dispatch = useDispatch();
+
+  const fetchUsers = () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    try {
+      let endpoint = "/users/mycompany/";
+      api.get(endpoint, config).then((res) => {
+        let response = res.data;
+        dispatch(setUserList(response));
+      });
+    } catch (error) {
+      console.log("nope");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [dispatch]);
+
+  return (
+    <RouteContentStyled>
+      <RouteHeadStyled>
+        <div>
+          <h2>Employees</h2>
+          <p>Here is the list of employees at the company.</p>
+        </div>
+        <ButtonBrand
+          iconURL={"/plus-add.svg"}
+          buttonText={"Add Employee"}
+          onClick={openModal}
+        />
+        <EmployeeForm isModalOpen={isModalOpen} closeModal={closeModal} />
+      </RouteHeadStyled>
+      <UserContainer />
+    </RouteContentStyled>
+  );
 }
--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -1,63 +1,71 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        accessToken: undefined,
-        role: "company_admin",
-        userObject: [], // this is the user object that will be fetched from the backend
-        isManager: undefined,
-        isCompanyAdmin: undefined,
-        userList: [
-            {
-                // User
-                id: 1,
-                firstName: "Bob",
-                lastName: "Sponge",
-                team: "Marketing",
-                profile: "/profile.png",
-                gender: "Male",
-                pensum: "100%",
-                manager: "3",
-            },
-            {
-                // User
-                id: 2,
-                firstName: "Roger V",
-                lastName: "Fed",
-                team: "Marketing",
-                profile: "/profile.png",
-                gender: "Male",
-                pensum: "100%",
-                manager: "4",
-            },
-        ],
+  name: "user",
+  initialState: {
+    accessToken: undefined,
+    role: "company_admin",
+    userObject: [], // this is the user object that will be fetched from the backend
+    isManager: undefined,
+    isCompanyAdmin: undefined,
+    userList: [
+      {
+        // User
+        id: 1,
+        firstName: "Bob",
+        lastName: "Sponge",
+        team: "Marketing",
+        profile: "/profile.png",
+        gender: "Male",
+        pensum: "100%",
+        manager: "3",
+      },
+      {
+        // User
+        id: 2,
+        firstName: "Roger V",
+        lastName: "Fed",
+        team: "Marketing",
+        profile: "/profile.png",
+        gender: "Male",
+        pensum: "100%",
+        manager: "4",
+      },
+    ],
+  },
+  reducers: {
+    login: (state, action) => {
+      state.accessToken = action.payload;
     },
-    reducers: {
-        login: (state, action) => {
-            state.accessToken = action.payload;
-        },
-        logout: (state) => {
-            state.accessToken = '';
-        },
-        setUserObject: (state, action) => {
-            state.userObject = action.payload;
-        },
-
-        setIsCompanyAdmin: (state, action) => {
-            state.isCompanyAdmin = action.payload;
-        },
-        setIsManager: (state, action) => {
-            if (action.payload)
-                state.isManager = true;
-        },
-
-        initialLoadLocalStorage: (state, action) => {
-            state.userList = action.payload;
-        },
-
+    logout: (state) => {
+      state.accessToken = "";
     },
+    setUserObject: (state, action) => {
+      state.userObject = action.payload;
+    },
+    setUserList: (state, action) => {
+      state.userList = action.payload;
+    },
+    setIsCompanyAdmin: (state, action) => {
+      state.isCompanyAdmin = action.payload;
+    },
+    setIsManager: (state, action) => {
+      if (action.payload) state.isManager = true;
+    },
+
+    initialLoadLocalStorage: (state, action) => {
+      state.userList = action.payload;
+    },
+  },
 });
 
-export const {login, logout, setUserObject, initialLoadLocalStorage, setIsCompanyAdmin, setIsManager} = userSlice.actions;
+export const {
+  login,
+  logout,
+  setUserObject,
+  setUserList,
+  initialLoadLocalStorage,
+  setIsCompanyAdmin,
+  setIsManager,
+} = userSlice.actions;
 export default userSlice.reducer;
