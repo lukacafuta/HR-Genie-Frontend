f9545939da0e0a479c43da2d5e62aff185911c32
Quentin Lamare <1092-qlaCode@users.noreply.gitlab.propulsion-home.ch>
2024-07-25 17:20:27 +0200

    Adjusted Request/Training Card fields, implemented minor formatting and text changes.

--- src/components/RequestCard.jsx
+++ src/components/RequestCard.jsx
@@ -1,189 +1,195 @@
-import {useDispatch, useSelector} from "react-redux";
-import {loadRequests, logRequestInfo} from "../store/slices/RequestSlice.jsx";
-import {RowCard} from "../styles/cardStyles.js";
-import {useEffect, useState} from "react";
-import {BtnGreen, BtnRed} from "../styles/buttonStyles.js";
+import { useDispatch, useSelector } from "react-redux";
+import { loadRequests, logRequestInfo } from "../store/slices/RequestSlice.jsx";
+import { RowCard } from "../styles/cardStyles.js";
+import { useEffect, useState } from "react";
+import { BtnGreen, BtnRed } from "../styles/buttonStyles.js";
 import ButtonGreen from "./buttons/ButtonGreen.jsx";
 import ButtonRed from "./buttons/ButtonRed.jsx";
-import {Link, useParams} from "react-router-dom";
-import {cleanUpIncomingDate, cleanUpIncomingText} from "../common/utils.js";
-import {api} from "../common/api.js";
+import { Link, useParams } from "react-router-dom";
+import { cleanUpIncomingDate, cleanUpIncomingText } from "../common/utils.js";
+import { api } from "../common/api.js";
 
-export default function RequestCard({oneRequest, updateRequests}) {
-    const {requestIndex} = useParams();
-    const view = useSelector((state) => state.view.view);
-    let currentRoleURLParam = view === "manager" ? "manager" : "employee";
+export default function RequestCard({ oneRequest, updateRequests }) {
+  const { requestIndex } = useParams();
+  const view = useSelector((state) => state.view.view);
+  let currentRoleURLParam = view === "manager" ? "manager" : "employee";
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const isManagerView = useSelector((state) => state.view.view) === "manager";
-    const accessToken = useSelector((state) => state.user.accessToken);
+  const isManagerView = useSelector((state) => state.view.view) === "manager";
+  const accessToken = useSelector((state) => state.user.accessToken);
 
-    // console.log("isManagerView", isManagerView);
+  // console.log("isManagerView", isManagerView);
 
-
-    const patchRequestStatus = (requestId, status) => {
-        const config = {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        };
-        const requestBody = {
-            status: status,
-        };
-        try {
-            api.patch(`/absences/manager/myteam/${requestId}`, requestBody, config).then((res) => {
-                console.log("API call successful", res.data);
-                updateRequests()
-
-
-            })
-        } catch (error) {
-            console.error(error);
-        }
+  const patchRequestStatus = (requestId, status) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    const requestBody = {
+      status: status,
+    };
+    try {
+      api
+        .patch(`/absences/manager/myteam/${requestId}`, requestBody, config)
+        .then((res) => {
+          console.log("API call successful", res.data);
+          updateRequests();
+        });
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    function handleMoreClick(requestCard) {
-        dispatch(logRequestInfo(requestCard));
-        setIsExpanded(!isExpanded);
-    }
+  function handleMoreClick(requestCard) {
+    dispatch(logRequestInfo(requestCard));
+    setIsExpanded(!isExpanded);
+  }
 
-    function handleDeny(requestCard) {
-        console.log("set to rejected", requestCard);
-        patchRequestStatus(requestCard.id, "rejected");
-    }
+  function handleDeny(requestCard) {
+    console.log("set to rejected", requestCard);
+    patchRequestStatus(requestCard.id, "rejected");
+  }
 
-    function handleApprove(requestCard) {
-        console.log("set to approved", requestCard);
-        patchRequestStatus(requestCard.id, "approved");
-    }
+  function handleApprove(requestCard) {
+    console.log("set to approved", requestCard);
+    patchRequestStatus(requestCard.id, "approved");
+  }
 
-    function handleUpdate(requestCard) {
-        console.log("update", requestCard);
-    }
+  function handleUpdate(requestCard) {
+    console.log("update", requestCard);
+  }
 
-    function handleDelete(requestCard) {
-        console.log("delete", requestCard);
-    }
+  function handleDelete(requestCard) {
+    console.log("delete", requestCard);
+  }
 
-    let {id, requester, startDt, endDt, dtCreated, reason, status} = oneRequest;
+  let {
+    id,
+    requester,
+    startDt,
+    endDt,
+    dtCreated,
+    reason,
+    status,
+    durationWorkHours,
+  } = oneRequest;
 
-    //Format Request Data
-    startDt = cleanUpIncomingDate(startDt);
-    endDt = cleanUpIncomingDate(endDt);
-    dtCreated = cleanUpIncomingDate(dtCreated);
-    reason = cleanUpIncomingText(reason);
-    status = cleanUpIncomingText(status);
+  //Format Request Data
+  startDt = cleanUpIncomingDate(startDt);
+  endDt = cleanUpIncomingDate(endDt);
+  dtCreated = cleanUpIncomingDate(dtCreated);
+  reason = cleanUpIncomingText(reason);
+  status = cleanUpIncomingText(status);
 
-    return (
-        <>
-            <Link to={`/${currentRoleURLParam}/requests/${id}#${id}`}>
-                <RowCard id={id} onClick={() => handleMoreClick(oneRequest)}>
-                    <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
+  return (
+    <>
+      <Link to={`/${currentRoleURLParam}/requests/${id}#${id}`}>
+        <RowCard id={id} onClick={() => handleMoreClick(oneRequest)}>
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
             <span>
               <img
-                  src="/profile.png"
-                  //src={requestCard.profile}
-                  alt="profile"
-                  height="35px"
-                  width="35px"
+                src="/profile.png"
+                //src={requestCard.profile}
+                alt="profile"
+                height="35px"
+                width="35px"
               />{" "}
             </span>
-                        <span>
+            <span>
               <b>
-                Requester {requester.customUser.username}
-                  {/*{requestCard.firstName} {requestCard.lastName}*/}
+                {requester.customUser.first_name}{" "}
+                {requester.customUser.last_name}
               </b>
             </span>
-                    </div>
-                    Request #{id}
-                    <span>{reason}</span>
-                    <span>
+          </div>
+          {reason}
+          <span>
             {startDt} - {endDt}
           </span>
-                    <div
-                        style={{
-                            display: "flex",
-                            justifySelf: "end",
-                            height: "100%",
-                            alignItems: "center",
-                        }}
-                    >
-                        {Number(requestIndex) !== id ? (
-                            <img src="/MoreInfo.png" alt="..." height="3px"/>
-                        ) : (
-                            <img src="/arrow-down.png" alt="..." height="8px"/>
-                        )}
-                    </div>
-                </RowCard>
-            </Link>
-            {Number(requestIndex) === id && (
-                <div style={{display: "flex", gap: "10px"}}>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "flex-start",
-                            gap: "10px",
-                            padding: "10px",
-                            width: "20%",
-                        }}
-                    >
+          <span>{durationWorkHours} hours</span>
+          <div
+            style={{
+              display: "flex",
+              justifySelf: "end",
+              height: "100%",
+              alignItems: "center",
+            }}
+          >
+            {Number(requestIndex) !== id ? (
+              <img src="/MoreInfo.png" alt="..." height="3px" />
+            ) : (
+              <img src="/arrow-down.png" alt="..." height="8px" />
+            )}
+          </div>
+        </RowCard>
+      </Link>
+      {Number(requestIndex) === id && (
+        <div style={{ display: "flex", gap: "10px" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              gap: "10px",
+              padding: "10px",
+              width: "20%",
+            }}
+          >
             <span>
-              {/*<b>Submitted on:</b> {requestCard.submittedOn}*/}
-                <b>Created on:</b> {dtCreated}
+              <b>Request No.:</b> {id}
             </span>
-                        <span>
+            <span>
+              <b>Created on:</b> {dtCreated}
+            </span>
+            <span>
               <b>Status:</b> {status}
             </span>
-                        {/*<span>*/}
-                        {/*  <b>Comment: </b>*/}
-                        {/*  {requestCard.comment}*/}
-                        {/*</span>*/}
-                    </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "end",
-                            gap: "10px",
-                            padding: "10px",
-                            width: "80%",
-                        }}
-                    >
-                        {(status === "Pending" && !isManagerView) && (
-                            <ButtonRed
-                                iconURL={"/cross-deny.png"}
-                                buttonText={"Delete"}
-                                onClick={() => handleDelete(oneRequest)}
-                            />
-                        )}
-                        {(status === "Pending" && !isManagerView) && (
-                            <ButtonGreen
-                                iconURL={"/tick-circle.png"}
-                                buttonText={"Update"}
-                                onClick={() => handleUpdate(oneRequest)}
-                            />
-                        )}
-
-                        {(status === "Pending" && isManagerView) && (
-                            <ButtonRed
-                                iconURL={"/cross-deny.png"}
-                                buttonText={"Deny"}
-                                onClick={() => handleDeny(oneRequest)}
-                            />
-                        )}
-                        {(status === "Pending" && isManagerView) && (
-                            <ButtonGreen
-                                iconURL={"/tick-circle.png"}
-                                buttonText={"Approve"}
-                                onClick={() => handleApprove(oneRequest)}
-                            />
-                        )}
-                    </div>
-                </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              gap: "10px",
+              padding: "10px",
+              width: "80%",
+            }}
+          >
+            {status === "Pending" && !isManagerView && (
+              <ButtonRed
+                iconURL={"/cross-deny.png"}
+                buttonText={"Delete"}
+                onClick={() => handleDelete(oneRequest)}
+              />
             )}
-        </>
-    );
+            {status === "Pending" && !isManagerView && (
+              <ButtonGreen
+                iconURL={"/tick-circle.png"}
+                buttonText={"Update"}
+                onClick={() => handleUpdate(oneRequest)}
+              />
+            )}
+
+            {status === "Pending" && isManagerView && (
+              <ButtonRed
+                iconURL={"/cross-deny.png"}
+                buttonText={"Deny"}
+                onClick={() => handleDeny(oneRequest)}
+              />
+            )}
+            {status === "Pending" && isManagerView && (
+              <ButtonGreen
+                iconURL={"/tick-circle.png"}
+                buttonText={"Approve"}
+                onClick={() => handleApprove(oneRequest)}
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- src/routes/manager.jsx
+++ src/routes/manager.jsx
@@ -1,36 +1,48 @@
-import {RouteContentStyled, RouteHeadStyled} from "../styles/routeGeneralStyles.js";
-import {useSelector} from "react-redux";
+import {
+  RouteContentStyled,
+  RouteHeadStyled,
+} from "../styles/routeGeneralStyles.js";
+import { useSelector } from "react-redux";
 import RequestMiniTable from "../components/RequestsMiniTable.jsx";
-import {RequestMiniTableAreaStyled} from "../styles/miniTableStyles.js";
+import { RequestMiniTableAreaStyled } from "../styles/miniTableStyles.js";
 
 export default function ManagerRoute() {
+  const requests = useSelector((state) => state.request.requestList);
+  const trainingRequests = useSelector((state) => state.request.trainingList);
+  // const vacationRequests = requests.filter(request => request.type === "Vacation");
+  // const trainingRequests = requests.filter(request => request.type === "Training");
+  // const absenceRequests = requests.filter(request => request.type === "Absence");
 
-    const requests = useSelector((state) => state.request.requestList);
-    const trainingRequests = useSelector((state) => state.request.trainingList);
-    // const vacationRequests = requests.filter(request => request.type === "Vacation");
-    // const trainingRequests = requests.filter(request => request.type === "Training");
-    // const absenceRequests = requests.filter(request => request.type === "Absence");
+  return (
+    <RouteContentStyled>
+      <RouteHeadStyled>
+        <div>
+          <h2>Manager Dashboard</h2>
+          <p>Here you can get an overview about the requests submitted.</p>
+        </div>
+      </RouteHeadStyled>
+      <h3>Calendar</h3>
+      <p>Calendar goes here</p>
+      <h3>Nice Charts</h3>
+      <p style={{ marginBottom: "2rem" }}>Charts go here</p>
 
-
-    return (
-        <RouteContentStyled>
-            <RouteHeadStyled>
-                <div><h2>Manager Dashboard</h2>
-                    <p>Add something here</p></div>
-
-            </RouteHeadStyled>
-            <h3>Calendar</h3>
-            <p>Calendar goes here</p>
-            <h3>Nice Charts</h3>
-            <p style={{marginBottom: "2rem"}}>Charts go here</p>
-
-            <RequestMiniTableAreaStyled>
-                <RequestMiniTable name={"Vacation Requests"} type={"vacation"} requests={requests}/>
-                <RequestMiniTable name={"Training Requests"} type={"training"} requests={trainingRequests}/>
-                <RequestMiniTable name={"Absence Notice"} type={"sick_leave"} requests={requests}/>
-            </RequestMiniTableAreaStyled>
-
-
-        </RouteContentStyled>
-    )
+      <RequestMiniTableAreaStyled>
+        <RequestMiniTable
+          name={"Vacation Requests"}
+          type={"vacation"}
+          requests={requests}
+        />
+        <RequestMiniTable
+          name={"Training Requests"}
+          type={"training"}
+          requests={trainingRequests}
+        />
+        <RequestMiniTable
+          name={"Absence Notice"}
+          type={"sick_leave"}
+          requests={requests}
+        />
+      </RequestMiniTableAreaStyled>
+    </RouteContentStyled>
+  );
 }
