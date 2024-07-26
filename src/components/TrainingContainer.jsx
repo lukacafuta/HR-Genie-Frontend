56f763d85ca71a8bb4895f6f0f79ceb03dd50532
Alexander Stöhr <git@alexander-stoehr.de>
2024-07-26 20:01:51 +0200

    killed a comment bug

--- src/components/TrainingContainer.jsx
+++ src/components/TrainingContainer.jsx
@@ -44,7 +44,7 @@ export default function TrainingContainer({refresh}) {
     const updateRequests = () => {
         fetchTrainings();
     };
-    created to refresh the requests
+    // created to refresh the requests
 
     useEffect(() => {
         fetchTrainings();
