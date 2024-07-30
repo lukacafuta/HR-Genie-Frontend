1e7510682ea21194c931b57901b50825c353b467
Quentin Lamare <1092-qlaCode@users.noreply.gitlab.propulsion-home.ch>
2024-07-30 23:48:47 +0200

    Adjusted RouteContentStyled padding for better alignment with navbar and Header Title

--- src/styles/routeGeneralStyles.js
+++ src/styles/routeGeneralStyles.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
-import {NavLink} from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 export const RouteContentStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0.5rem 2rem 2rem 3.2rem;
 `;
 
 export const RouteHeadStyled = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding-bottom: 2rem;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-bottom: 2rem;
+  align-content: end;
 
-    p {
-        color: var(--dark-grey);
-    }
+  p {
+    color: var(--dark-grey);
+  }
 `;
